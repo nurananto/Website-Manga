b22d0eb0187ee9360e2aed4556ea78eee7953e70
nurananto <34748019+nurananto@users.noreply.github.com>
2026-06-11 11:23:44 +0700

    fix: add PATCH to CORS allowed methods

--- worker/api-worker.js
+++ worker/api-worker.js
@@ -18,7 +18,7 @@
 
 const CORS = {
   'Access-Control-Allow-Origin':  '*',
-  'Access-Control-Allow-Methods': 'GET, HEAD, POST, DELETE, OPTIONS',
+  'Access-Control-Allow-Methods': 'GET, HEAD, POST, PATCH, DELETE, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type, Authorization',
 };
 
